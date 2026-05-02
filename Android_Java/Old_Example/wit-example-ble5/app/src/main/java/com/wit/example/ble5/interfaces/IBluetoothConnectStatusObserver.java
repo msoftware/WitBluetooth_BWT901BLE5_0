857fb8acb115ff761c81ba2be3795fcb7df8b625
Michael <michael@jentsch.io>
2026-05-02 19:22:12 +0200

    Cleanup

--- Android_Java/Old_Example/wit-example-ble5/app/src/main/java/com/wit/example/ble5/interfaces/IBluetoothConnectStatusObserver.java
+++
@@ -1,7 +0,0 @@
-package com.wit.example.ble5.interfaces;
-
-// 状态观察者 Status Observer
-public interface IBluetoothConnectStatusObserver {
-    // 状态改变时 When the state changes
-    void onStatusChanged(String mac, int status);
-}
--- IOS_swift/WitSDK/Sensor/Utils/SyncDictionary.swift
+++
@@ -1,8 +0,0 @@
-//
-//  线程安全的字典
-//  Created by huangyajun on 2022/9/2.
-//
-
-import Foundation
-
-
